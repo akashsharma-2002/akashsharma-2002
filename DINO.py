c067eb471b9d51583e964afc76b80d65c1dd0106
Akash Sharma <73756172+akashsharma-2002@users.noreply.github.com>
2021-05-23 16:38:07 +0530

    Add files via upload

---
+++ DINO.py
@@ -0,0 +1,73 @@
+import pyautogui
+from win32com.client import Dispatch
+from PIL import Image,ImageGrab
+import time
+from numpy import asarray
+
+replay_button=(683,406)
+dino=(142,439)
+speak=Dispatch("SAPI.spVOICE")
+speak.speak("your game is about to start in another 3 second")
+speak.speak("hold back, and enjoy")
+speak.speak("three")
+speak.speak("two")
+speak.speak("one")
+
+
+def hit():
+    pyautogui.click(replay_button)
+def space():
+    pyautogui.keyDown("up")
+def down():
+    pyautogui.keyDown("down")
+"""def black_cac():
+    image=dino[0]+50,dino[1],dino[0]+220,dino[1]+41
+    img=ImageGrab.grab(image).convert('L')
+    b=asarray(img)
+    print(b.sum())
+    return(b.sum())
+
+def black_bird():
+    imag=dino[0]+150,dino[1]-36,dino[0]+290,dino[1]-28
+    img=ImageGrab.grab(imag).convert('L')
+    b=asarray(img)
+    print(b.sum())
+    return(b.sum())"""
+
+def bird():
+    imag=dino[0]+150,dino[1]-36,dino[0]+280,dino[1]-28
+    img=ImageGrab.grab(imag).convert('L')
+    b=asarray(img)
+    print(b.sum())
+    return(b.sum())
+
+def collide():
+    image=dino[0]+50,dino[1],dino[0]+260,dino[1]+41
+    img=ImageGrab.grab(image).convert('L')
+    b=asarray(img)
+    print(b.sum())
+    return(b.sum())
+
+#bird= 816000
+hit()
+
+while(True):
+    collide()
+    bird()
+    #black_cac()
+    #black_bird()
+    if(collide()!=2195550): #2195550
+        space()
+
+    elif(bird()!=265200 ):
+        space()
+
+    """elif(black_bird()==0):
+        if(black_bird()!=0):
+            space()
+
+    elif(black_cac()==0):
+        if(black_cac()!=0):
+                space()
+"""
+
